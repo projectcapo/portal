79ace3edd618f1b1009f06439a61f49b9f349188
Tim McGowan <30318479+timmcgowan@users.noreply.github.com>
2018-08-26 19:43:56 -0400

    updated userint logic

--- js/auth.js
+++ js/auth.js
@@ -168,7 +168,7 @@ function sendPasswordReset() {
  *    out, and that is where we update the UI.
  */
 
-
+/*
 function initUser() {
 
     var user = firebase.auth().currentUser;
@@ -188,7 +188,44 @@ function initUser() {
     }
 
 }
+*/
 
+var initUser = function() {
+    var accountdetails;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var uid = user.uid;
+        var phoneNumber = user.phoneNumber;
+        var providerData = user.providerData;
+        user.getIdToken().then(function(accessToken) {
+          document.getElementById('sign-in-status').textContent = 'Signed in';
+          //document.getElementById('sign-in').textContent = 'Sign out';
+        accountdetails = JSON.stringify({ displayName: displayName,
+            email: email,
+            emailVerified: emailVerified,
+            phoneNumber: phoneNumber,
+            photoURL: photoURL,
+            uid: uid,
+            accessToken: accessToken,
+            providerData: providerData
+          }, null, '  ');
+          document.getElementById('account-details').textContent = accountdetails;
+        });
+      } else {
+        // User is signed out.
+        document.getElementById('sign-in-status').textContent = 'Signed out';
+     //   document.getElementById('sign-in').textContent = 'Sign in';
+        document.getElementById('account-details').textContent = 'null';
+      }
+    }, function(error) {
+      console.log(error);
+    });
+  };
 
 function initApp() {
     // Listening for auth state changes.
