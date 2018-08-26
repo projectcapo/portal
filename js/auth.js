d6247b9ac3e871d76cb52c63dbe5a2b1e42c4d5e
Tim McGowan <30318479+timmcgowan@users.noreply.github.com>
2018-08-26 18:48:25 -0400

    updated auth logic

--- js/auth.js
+++ js/auth.js
@@ -20,17 +20,229 @@ var config = {
     projectId: "capo-cbd68",
     storageBucket: "capo-cbd68.appspot.com",
     messagingSenderId: "1023788850417"
-  };
-  firebase.initializeApp(config);
+};
 
-  var dataRef = firebase.database();
-  var auth = firebase.auth();
+firebase.initializeApp(config);
 
-// Initial Values
+var dataRef = firebase.database();
+var auth = firebase.auth();
+
+// Initial Global Values
 var name = "";
-var email = "";
 var company = "";
 var comment = "";
+
+var displayName;
+var email;
+var emailVerified;
+var photoURL;
+var uid;
+var providerData;
+
+/**
+ * Handles the sign in button press.
+ */
+function toggleSignIn() {
+    if (firebase.auth().currentUser) {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+    } else {
+        var email = $("#email-login").val();
+        var password = $("#password-login").val();
+
+        if (email.length < 4) {
+            alert('Please enter an email address.');
+            return;
+        }
+        if (password.length < 4) {
+            alert('Please enter a password.');
+            return;
+        }
+
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(function () {
+                // Existing and future Auth states are now persisted in the current
+                // session only. Closing the window would clear any existing state even
+                // if a user forgets to sign out.
+                // ...
+                // New sign-in will be persisted with session persistence.
+                return firebase.auth().signInWithEmailAndPassword(email, password);
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // [START_EXCLUDE]
+                if (errorCode === 'auth/wrong-password') {
+                    alert('Wrong password.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+
+                //document.getElementById('quickstart-sign-in').disabled = false;
+                // [END_EXCLUDE]
+            });
+        // [END authwithemail]
+    }
+    //document.getElementById('quickstart-sign-in').disabled = true;
+    $(".logout-user").show()
+    $(".login-user").hide()
+    $("#sign-up-header").hide()
+}
+
+/**
+ * Handles the sign up button press.
+ */
+function handleSignUp() {
+    var email = $("#email-login").val();
+    var password = $("#password-login").val();
+    if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+    }
+    if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+    }
+    // Sign in with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+    });
+    // [END createwithemail]
+}
+
+/**
+ * Sends an email verification to the user.
+ */
+function sendEmailVerification() {
+    // [START sendemailverification]
+    firebase.auth().currentUser.sendEmailVerification().then(function () {
+        // Email Verification sent!
+        // [START_EXCLUDE]
+        alert('Email Verification Sent!');
+        // [END_EXCLUDE]
+    });
+    // [END sendemailverification]
+}
+
+function sendPasswordReset() {
+    var email = document.getElementById('email').value;
+    // [START sendpasswordemail]
+    firebase.auth().sendPasswordResetEmail(email).then(function () {
+        // Password Reset Email Sent!
+        // [START_EXCLUDE]
+        alert('Password Reset Email Sent!');
+        // [END_EXCLUDE]
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/invalid-email') {
+            alert(errorMessage);
+        } else if (errorCode == 'auth/user-not-found') {
+            alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+    });
+    // [END sendpasswordemail];
+}
+
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+
+
+function initUser() {
+
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+        user.providerData.forEach(function (profile) {
+            console.log("Sign-in provider: " + profile.providerId);
+            console.log("  Provider-specific UID: " + profile.uid);
+            console.log("  Name: " + profile.displayName);
+            console.log("  Email: " + profile.email);
+            console.log("  Photo URL: " + profile.photoURL);
+        });
+    } else {
+        // No user is signed in.
+        console.log("not signed in")
+        //window.location.replace("./login.html")
+    }
+
+}
+
+
+function initApp() {
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function (user) {
+        // [START_EXCLUDE silent]
+        //    document.getElementById('quickstart-verify-email').disabled = true;
+        // [END_EXCLUDE]
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // [START_EXCLUDE]
+            /* document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            if (!emailVerified) {
+              document.getElementById('quickstart-verify-email').disabled = false;
+            }
+            */
+            $(".logout-user").show()
+            $(".login-user").hide()
+            $("#sign-up-header").hide()
+            // [END_EXCLUDE]
+        } else {
+            // User is signed out.
+            // [START_EXCLUDE]
+            $(".logout-user").hide()
+            $(".login-user").show()
+            $("#sign-up-header").show()
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+            //   document.getElementById('quickstart-sign-in').textContent = 'Sign in';
+            //  document.getElementById('quickstart-account-details').textContent = 'null';
+            // [END_EXCLUDE]
+        }
+        // [START_EXCLUDE silent]
+        // document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+    });
+    // [END authstatelistener]
+
+    //document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+    //document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
+    //document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
+    //document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+}
+
+
+
 /*
 // Capture Button Click
 $("#add-user").on("click", function (event) {
@@ -95,90 +307,31 @@ dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functio
     $("#comment-display").text(snapshot.val().comment);
 });*/
 
-function login(email,password){
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-    .then(function() {
-      // Existing and future Auth states are now persisted in the current
-      // session only. Closing the window would clear any existing state even
-      // if a user forgets to sign out.
-      // ...
-      // New sign-in will be persisted with session persistence.
-      return firebase.auth().signInWithEmailAndPassword(email, password);
-    })
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
-}
-/*
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      console.log("user :" + user)
-    } else {
-      // No user is signed in.
-      console.log("not signed in")
-    }
-  });
-*/
-  var user = firebase.auth().currentUser;
-
-  if (user) {
-      // User is signed in.
-      console.log("user :" + user)
-
-  } else {
-      // No user is signed in.
-      console.log("not signed in")
-  }
 
 $(document).ready(function () {
     $(".login-user").on("click", function (event) {
         event.preventDefault();
-        var email = $("#email-login").val();
-        var password = $("#password-login").val();
-
-        console.log(email, password);
-        login(email,password);
-/*        
-        var promise = auth.signInWithEmailAndPassword(email, password).catch(function (error) {
-
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorCode)
-            console.log(errorMessage)
-            // ...
-        });
-
-        promise.then(function (value) {
-            console.log(value);
-        });
-        */
-       console.log(firebase.auth().currentUser);
-    });
-
+        toggleSignIn();
+    })
 });
-  
-$(".logout-user").on("click", function (event) {
-    event.preventDefault();
 
-    var promise = auth.signOut().catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode)
-        console.log(errorMessage)
-        // ...
-    });
-
-    promise.then(function(value) {
-        console.log(value);
+$(document).ready(function () {
+    $(".logout-user").on("click", function (event) {
+        event.preventDefault()
+        event.stopPropagation()
+        firebase.auth().signOut()
+            .then(function () {
+                // Sign-out successful.
+                console.log("sign-out")
+            })
+            .catch(function (error) {
+                // An error happened
+                console.log("ERR MSG" + error)
+            });
     });
 });
 
-auth.onAuthStateChanged(function(user) {
+auth.onAuthStateChanged(function (user) {
     if (user) {
         $(".logout-user").show()
         $(".login-user").hide()
@@ -188,4 +341,4 @@ auth.onAuthStateChanged(function(user) {
         $(".login-user").show()
         $("#sign-up-header").show()
     }
-  });
+});
